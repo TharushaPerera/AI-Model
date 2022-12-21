558c21d9cc507b2360f2ad17f742e02885fba464
WarunaDisanayaka <89345041+TharushaPerera@users.noreply.github.com>
2022-12-21 22:45:24 +0530

    Update with some function speech recognition

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 #Libraries
 import pyttsx3
+import speech_recognition as sr   
 
 engine = pyttsx3.init('sapi5')
 voice=engine.getProperty('voice')
 engine.setProperty('voice',voice[1].id)
+
+
+#Speak function
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+if __name__=="__main__":
+    speak('Hello I am your personal assistant')
+
+
+def takeVoiceCommand():
+    r=sr.Recognizer()
+

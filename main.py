929c8b3edc043378c3d0598ad66f29e64af8c939
WarunaDisanayaka <89345041+TharushaPerera@users.noreply.github.com>
2023-02-01 23:57:44 +0530

    Updated

--- main.py
+++ main.py
@@ -72,16 +72,6 @@ def response(text):
 
 
 
-def call(text):
-    action_call = "assistant"
-
-    text = text.lower()
-
-    if action_call in text:
-        return True
-
-    return False
-
 
 
 
